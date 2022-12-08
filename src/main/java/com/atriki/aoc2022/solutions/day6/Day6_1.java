d37e2647b035daccebd27bbbe72d4771897b02d7
Ali Triki <alitriki2@gmail.com>
2022-12-08 19:22:07 +0100

    day 6

---
+++ src/main/java/com/atriki/aoc2022/solutions/day6/Day6_1.java
@@ -0,0 +1,29 @@
+package com.atriki.aoc2022.solutions.day6;
+
+import com.atriki.aoc2022.Day;
+
+public class Day6_1 extends Day<Integer> {
+
+    public Day6_1() {
+        super(6);
+    }
+
+    @Override
+    public Integer process() {
+        return findFirstFourContiguousCharacters(allLines().get(0));
+    }
+
+    public int findFirstFourContiguousCharacters(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            boolean charactersAreDifferent = checkAllCharactersAreDifferent(input.substring(i, i + 4));
+            if (charactersAreDifferent) {
+                return i + 4;
+            }
+        }
+        throw new IllegalArgumentException("¿qué pasa?");
+    }
+
+    private boolean checkAllCharactersAreDifferent(String characters) {
+        return characters.length() == characters.chars().distinct().count();
+    }
+}
